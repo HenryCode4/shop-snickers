1c785c40b8ae26f700c8256401780c9c1719ae7e
HenryCode4 <henrycnnamani4@gmail.com>
2023-08-20 23:27:42 -0700

    changes in footer

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,7 +111,7 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "customer@shopsnickers.com", link: "mailto:customer@shopsnickers.com" },
             { name: "+92554862354", link: "tel:+92554862354" },
         ],
     },
